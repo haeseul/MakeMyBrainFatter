e4a3bf486dbd723c74d851cfab5ebf8cdcd020ea
haeseul <ksw06205@naver.com>
2022-05-26 13:19:46 +0900

    tensor class

--- test.py
+++ test.py
@@ -1,18 +1,70 @@
-# Python을 사용하여 다음 지침에 따라 모듈을 가져오지 않고 클래스를 구성합니다.
-# - 데이터와 모양의 2가지 입력이 주어지면 중첩 목록을 사용하여 텐서를 구성합니다.
-# - 텐서는 n차원 행렬 (순서는 스칼라, 벡터, 행렬, 텐서로 이동)
-# - data[] : 모든 숫자(int 또는 float)
-# - shape[]: 양의 정수 -> shape[] = empty list면 [] 출력
-# -구성된 텐서는 인스턴스 변수로 저장하거나 표준 출력으로 인쇄하거나 둘 다 할 수 있습니다.
+# Python을 사용하여 다음 지침에 따라 라이브러리(TENSORFLOW, NUMPY, OR SCIPY)를 가져오지 않고 클래스를 구성하시오
+# - data와 shape이 주어지면 중첩 list을 사용하여 텐서 함수를 구성
+# - 텐서는 n차원 행렬 (행렬 순서는 스칼라, 벡터, 행렬, 텐서)
+# - data[] : 모든 숫자(int 또는 float), 길이 제한X
+# - shape[]: 양의 정수 -> shape이 empty list면 [] 출력, 길이 제한X
+# - 구성된 텐서는 인스턴스 변수로 저장하거나 표준 출력으로 인쇄하거나 둘 다
 # - 데이터 넘치면 자르기, 남으면 0으로 채우기
+
+
 
 class Tensor():
     def __init__ (self, data, shape):
         self.data = data
         self.shape = shape
-        self.tensor = 
+        self.tensor = Tensor.shape_data(self)
 
-    def shape_data(self, ): 
+    def shape_data(self):
+        m = 0
+        if self.shape == []:
+            return []
+
+        ## Tensor
+        elif len(self.shape) == 3:
+            self.tensor = [[[0 for k in range(self.shape[2])] for j in range(self.shape[1])] for i in range(self.shape[0])]
+            if self.data == []:
+            return self.tensor
+            else:
+            for i in range(len(self.tensor)):
+                for j in range(len(self.tensor[i])):
+                for k in range(len(self.tensor[i][j])):
+                    self.tensor[i][j][k] = self.data[m]
+                    m += 1
+            return self.tensor
+
+        ## Matrix
+        elif len(self.shape) == 2:
+            self.tensor = [[0 for k in range(self.shape[1])] for j in range(self.shape[0])]
+            if self.data == []:
+            return self.tensor
+            else:
+            for i in range(len(self.tensor)):
+                for j in range(len(self.tensor[i])):
+                self.tensor[i][j] = self.data[m]
+                m += 1
+            return self.tensor
+
+        elif len(self.shape) == 1:
+            ## Vector
+            if self.shape != [1]:
+            self.tensor = [0 for k in range(self.shape[0])]
+            if self.data == []:
+                return self.tensor
+            else:
+                for i in range(len(self.tensor)):
+                self.tensor[i] = self.data[m]
+                m += 1
+                return self.tensor
+
+            ## Scalar
+            else:
+            if self.data == []:
+                return 0
+            else:
+                return self.data[0]
+
+        else:
+            raise ValueError
 
 
 
@@ -21,7 +73,7 @@ class Tensor():
 >>> tensor0 = Tensor(data0, shape0)
 
 output:
-[[[0, 1], [2, 3], [4, 5]], [[0.1, 0.2], [-3, 0], [0, 0]]] --> 12개 -> 2등분 -> 3등분 -> 2개씩
+[[[0, 1], [2, 3], [4, 5]], [[0.1, 0.2], [-3, 0], [0, 0]]]
 
 
 >>> data1 = [0, 1, 2, 3, 4, 5, 0.1, 0.2, -3, -2, -1, 3, 2, 1]
@@ -29,12 +81,5 @@ output:
 >>> tensor1 = Tensor(data1, shape1)
 
 output:
-[[0, 1], [2, 3], [4, 5], [0.1, 0.2], [-3, -2]] --> 10개만 -> 5등분 -> 2개씩
+[[0, 1], [2, 3], [4, 5], [0.1, 0.2], [-3, -2]]
 
-
->>> data3 = [0, 1, 2, 3, 4, 5, 0.1, 0.2, -3, -2, -1, 3, 2, 1] -> 14개
->>> shape3 = [5,4,3,2]
->>> tensor3 = Tensor(data3, shape3) -> 120개 가능
-
-output:
-[[[[][][]][[][][]][[][][]][[][][]]],[[][][][]],[[][][][]],[[][][][]],[[][][][]]]
