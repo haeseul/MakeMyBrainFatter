4aa636743323d2739bef8acfb5cd0d345946a907
haeseul <58204031+haeseul@users.noreply.github.com>
2023-12-03 10:37:12 +0900

    Add files via upload

---
+++ data_structure/ConnectedComponent_BFS.py
@@ -0,0 +1,37 @@
+def bfs(v, num, visited, graph):
+    q = [v]
+    while q:
+        node = q.pop(0)
+        if not visited[node]:
+            visited[node] = True
+            num[0] += 1
+            q.extend(graph[node])
+    return visited
+
+def connected(graph, size):
+    visited = [False for _ in range(size+1)]
+    nums = []
+    for v in range(1, size+1):
+        if not visited[v]:
+            num = [0]
+            bfs(v, num, visited, graph)
+            nums.append(num[0])
+    return nums
+
+if __name__ == "__main__":
+    numTestCases = int(input())
+
+    for _ in range(numTestCases):
+        n = int(input())
+        graph = [[] for _ in range(n+1)]
+        for i in range(n):
+            adjacency = list(map(int, input().split()))
+            vertex = adjacency.pop(0)
+            graph[vertex] = adjacency[1:]
+
+        nums = connected(graph, vertex)
+        nums = sorted(nums)
+        print(len(nums), end=' ')
+        for i in nums:
+            print(i, end=' ')
+        print()
